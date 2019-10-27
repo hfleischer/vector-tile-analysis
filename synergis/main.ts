d804d6da6acb3be8afe850c6536a1c9e9484862c
h.fleischer@mysynergis.com <h.fleischer@mysynergis.com>
2019-10-27 09:31:44 +0100

    test

--- synergis/main.ts
+++ synergis/main.ts
@@ -12,28 +12,13 @@ ProtocolTypesVectortile.init();
 VectorTileAnalysisApp.initLayout();
 VectorTileAnalysisApp.initMap();
 
-//items are stacked bottom in insertion order, last item added will be top item in the map
-/*
-VectorTileAnalysisApp.addLayerSet({
-    id: 'ags_basemap',
-    title: 'World_Basemap_v2',
-    url: 'https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer'
-});
-*/
-/*
-VectorTileAnalysisApp.addLayerSet({
-    id: 'syn_basemap_20190220',
-    title: 'bmapv_20190220',
-    url: 'http://w-lap-fleischer.synergis.intern/vt_20190220/p12/resources/styles/root.json'
-});
-*/
-
 VectorTileAnalysisApp.addLayerSet({
     visible: true,
     id: 'ags_basemap',
     title: 'World_Basemap_v2',
     url: 'https://basemaps.arcgis.com/arcgis/rest/services/World_Basemap_v2/VectorTileServer'
 });
+/*
 VectorTileAnalysisApp.addLayerSet({
     visible: false,
     id: 'syn_basemap_20181220',
@@ -46,6 +31,7 @@ VectorTileAnalysisApp.addLayerSet({
     title: 'bmapv_20190220',
     url: 'http://w-lap-fleischer.synergis.intern/vt_20190220/p12/resources/styles/root.json'
 });
+*/
 
 
 VectorTileAnalysisApp.initMapDetails();
